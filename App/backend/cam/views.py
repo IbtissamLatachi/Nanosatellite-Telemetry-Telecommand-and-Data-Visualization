7a409959ce300bc17fa39b47c5a2af3eb9ac6edc
mahmoud21a <107002413+mahmoud21a@users.noreply.github.com>
2023-12-12 19:04:23 +0100

    Fixed Security Risk

--- App/backend/cam/views.py
+++ App/backend/cam/views.py
@@ -18,7 +18,7 @@ def cam_view(request):
 
     try:
         client = pymongo.MongoClient(
-            "mongodb+srv://merradi:sonxoq-xubfi5-tyxPas@cluster0.uwxph0t.mongodb.net/?retryWrites=true&w=majority",
+            "",
             tlsCAFile=certifi.where(),
         )
         db = client.myDatabase
