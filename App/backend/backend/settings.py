48e90f4bfb5600611422d63c4779a4c7ff7e02d3
mahmoud21a <107002413+mahmoud21a@users.noreply.github.com>
2023-12-12 19:07:31 +0100

    Fixed Security Risk

--- App/backend/backend/settings.py
+++ App/backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7efv7_uzq@*pd4@eg^$3v31-))m)j5)cjs14ssje@b9-epvc=e"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
